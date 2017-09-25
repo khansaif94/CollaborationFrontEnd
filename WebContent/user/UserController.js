b460584e045e5df3d5ab70bb60f00105285f59cc
khansaif94 <khansaif94@gmail.com>
2017-09-25 11:46:43 +0530

    chat completed

--- WebContent/user/UserController.js
+++ WebContent/user/UserController.js
@@ -1,12 +1,30 @@
-app.controller("userController",['$scope','UserService','$location','$rootScope',
-		function($scope, UserService, $location, $rootScope) {
+app.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+app.controller("userController",['$scope','UserService','$location','$rootScope','$cookies',
+		function($scope, UserService, $location, $rootScope, $cookies) {
+	
 	
 	var self = this;
 	$scope.users=[];
-	$scope.user={
+	$rootScope.friendprofile=[];
+	$rootScope.userprofile=[];
+	self.buser={
 			errorCode: '',
 			errorMessage: '',
-			userid: '',
+			id: '',
 			email: '',
 			userrole: '',
 			dob: '',
@@ -15,14 +33,29 @@ app.controller("userController",['$scope','UserService','$location','$rootScope'
 			password: '',
 			phoneno: '',
 			gender: '',
-			profilepic: ''
+			imagepath: ''
+			};
+	$scope.user={
+			errorCode: '',
+			errorMessage: '',
+			id: '',
+			email: '',
+			userrole: '',
+			dob: '',
+			firstname: '',
+			lastname: '',
+			password: '',
+			phoneno: '',
+			gender: '',
+			imagepath: ''
 			};
 	
 	self.getAllUser=function(){
 		console.log('retriving userss...');
-		UserService.getAllUser().then(
+		UserService.getAllUser()
+		.then(
 		function(data){
-			$scope.user=data;
+			$scope.users=data;
 		},
 		function(errResponse){
 			console.log('Error in retriving user');
@@ -32,6 +65,72 @@ app.controller("userController",['$scope','UserService','$location','$rootScope'
 		
 	};
 	
+	self.searchpeople=function()
+	{
+		console.log("searchpeople");
+		UserService.search($scope.name)
+		.then(
+				function(u)
+				{
+					console.log(u);
+					$scope.searchresults=u;
+				},
+				function(errResponse)
+				{
+					console.log("error");
+				}
+			 );
+};
+	
+self.getuser=function(id)
+{
+	console.log('in user cntrl getuser method ...'+id);
+	
+	UserService.getuser(id)
+	.then(
+			function(response)
+			{
+				//console.log(response);
+				self.buser=response;
+			},
+			function(errResponse)
+			{
+				console.error('Error while retrieving user');
+			})
+};
+
+
+self.getuserByName=function(nm)
+{
+	console.log('in user cntrl getuser method ...'+nm);
+	UserService.getuserByName(nm)
+	.then(
+			function(response)
+			{
+				
+			},
+			function(errResponse)
+			{
+				console.error('Error while retrieving user');
+			})
+};
+	
+self.getUserById=function(userid)
+{
+	console.log('in user cntrl getUserById method ...'+userid);
+	UserService.getUserById(userid)
+	.then(
+			function(response)
+			{
+				
+			},
+			function(errResponse)
+			{
+				console.error('Error while retrieving user');
+			})
+};
+
+
 	self.saveUser=function(){
 		console.log('adding new user...');
 		UserService.saveUser($scope.user).then(
@@ -48,16 +147,15 @@ app.controller("userController",['$scope','UserService','$location','$rootScope'
 		
 	};
 	
-	this.validate = function(user) {
-		console.log("authenticate...");
-		console.log(this.user);
+	self.validate = function() {
+		console.log("authenticating...");
+		console.log($scope.user);
 		UserService
-				.validate(this.user)
+				.validate($scope.user)
 				.then(
 
 						function(data) {
 								console.log(data);
-								console.log("akku-niyu:"+$scope.user);
 							$scope.user = data;
 							console.log($scope.user);
 							console
@@ -76,20 +174,14 @@ app.controller("userController",['$scope','UserService','$location','$rootScope'
 								console
 										.log("Valid credentials. Navigating to home page");
 
-								if($scope.user.role=="Admin")	
-									{
-									console.log("You are admin");
-									$scope.fetchAllUsers();
-									console.log("i m tired...");
-									}
 								
 
-								console.log('Current user : '+user);
+								console.log('Current user : '+$scope.user);
 								$rootScope.currentUser = $scope.user;
 								console.log($rootScope.currentUser);
-								$cookieStore.put('currentUser',$rootScope.currentUser);
-								console.log($rootScope.currentUser.role);
-								$http.defaults.headers.common['Authorization'] = 'Basic' + $rootScope.currentUser;								
+								$cookies.put('currentUser',$rootScope.currentUser);
+								console.log($rootScope.currentUser.userrole);
+									
 								$location.path('/');
 							}
 
@@ -101,4 +193,44 @@ app.controller("userController",['$scope','UserService','$location','$rootScope'
 						});
 	};
 	
+	self.uploadFile=function(){
+		  console.log("uploading file");
+		  var file =  $scope.myfile;
+			  console.log(file);
+			  var fd = new FormData();
+			   fd.append('file',file);
+			   UserService.uploadFile(fd,$rootScope.currentUser.id)
+				.then(
+						function(response)
+						{
+							console.log(response);
+							alert("Image uploaded successfully");
+						},
+						function(errResponse)
+						{
+							console.log("Error while uploading profile picture");
+						}
+					  );
+			
+			};
+			
+			self.logout=function()
+			{
+				console.log("logging out current user");
+				UserService.logout()
+					.then(
+							function()
+							{
+								$rootScope.currentUser='';
+								$cookies.remove('currentUser');
+								alert("Logged out successfully");
+								$location.path("/login");
+							},
+							function(errResponse){
+								console.error('Error while logging out');
+			                });
+			
+			}
+			
+
 } ]);
